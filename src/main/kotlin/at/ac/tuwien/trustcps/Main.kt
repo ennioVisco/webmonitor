e17ef0fe12a218afba53a1a88691a50f5937f5d5
Ennio Visconti <ennio.visconti@gmail.com>
2022-01-21 02:45:44 +0100

    further cleanup

--- src/main/kotlin/at/ac/tuwien/trustcps/Main.kt
+++ src/main/kotlin/at/ac/tuwien/trustcps/Main.kt
@@ -1,16 +1,64 @@
 package at.ac.tuwien.trustcps
 
+import at.ac.tuwien.trustcps.checker.Checker
+import at.ac.tuwien.trustcps.tracker.Browser
 import com.tylerthrailkill.helpers.prettyprint.pp
 import at.ac.tuwien.trustcps.tracker.PageTracker
+import eu.quanticol.moonlight.formula.AtomicFormula
+import eu.quanticol.moonlight.formula.Formula
+import eu.quanticol.moonlight.signal.SpatialTemporalSignal
+import org.openqa.selenium.Dimension
 import java.net.URL
+import java.util.*
+
+private const val WIDTH = 320
+private const val HEIGHT = 280
+private const val URL = "https://tuwien.ac.at/"
 
 fun main() {
-    val baseUrl = URL("https://www.tuwien.ac.at/")
-
-   val tracker = PageTracker(baseUrl)
-    tracker.select("h1")
-    tracker.select("h2")
-
-    val data = tracker.track()
+    print("Starting tracking...")
+    println(Calendar.getInstance().time)
+    val data = tracking()
     data.pp()
+
+    print("Starting checking...")
+    println(Calendar.getInstance().time)
+    checking(data, spec())
+
+//    val output = Reporter()
+//    output.report(result, "fromJava Quantitative")
+
+    print("Ending...")
+    println(Calendar.getInstance().time)
 }
+
+private fun spec(): Formula {
+    return AtomicFormula("#cookieman-modal p")
+}
+
+private fun tracking(): Map<String, String> {
+    val baseUrl = URL(URL)
+
+    val tracker = PageTracker(baseUrl, Dimension(WIDTH, HEIGHT), Browser.CHROME)
+    tracker.select("#cookieman-modal p")
+
+    return tracker.track()
+}
+
+private fun checking(data: Map<String, String>, spec: Formula)
+        : SpatialTemporalSignal<Boolean>?
+{
+    val checker = Checker(WIDTH, HEIGHT, data)
+    return checker.check(spec)
+}
+
+//{
+//    "wnd_height" -> "1032",
+//    "#cookieman-modal p::y" -> "122",
+//    "#cookieman-modal p::x" -> "31",
+//    "vp_height" -> "147",
+//    "#cookieman-modal p::height" -> "161",
+//    "vp_width" -> "500",
+//    "wnd_width" -> "1920",
+//    "#cookieman-modal p::width" -> "421"
+//}
