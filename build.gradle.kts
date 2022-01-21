d48580e96e6071636e3337200b1df8bde473a75c
Ennio Visconti <ennio.visconti@gmail.com>
2022-01-21 02:52:41 +0100

    tentative jacoco fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,9 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+    }
 }
 
 
