2a206e149ebe3d7bd4f7bb6bb709c299a3ddf30c
Ennio Visconti <ennio.visconti@gmail.com>
2022-02-01 17:07:52 +0100

    disabled windows-dependant test

--- src/test/kotlin/at/ac/tuwien/trustcps/tracking/SessionBuilderTest.kt
+++ src/test/kotlin/at/ac/tuwien/trustcps/tracking/SessionBuilderTest.kt
@@ -1,12 +1,14 @@
 package at.ac.tuwien.trustcps.tracking
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.chrome.ChromeDriver
 import java.net.URL
 
 internal class SessionBuilderTest {
+    @Disabled
     @Test fun `browser session is initialized correctly`() {
         val url = URL("https://google.com")
         val dims = Dimension(800, 600)
