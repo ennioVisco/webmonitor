860cb98403135ee34dfe00d4e20a7efe06a968f3
Ennio Visconti <ennio.visconti@gmail.com>
2022-02-02 23:36:09 +0100

    updated class docs

--- src/main/kotlin/at/ac/tuwien/trustcps/tracking/PageTracker.kt
+++ src/main/kotlin/at/ac/tuwien/trustcps/tracking/PageTracker.kt
@@ -5,48 +5,59 @@ import org.openqa.selenium.Dimension
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URL
 
-class PageTracker(private val targetPage: URL,
+/**
+ * Tracks key elements of the target page.
+ * @param page is the url to track
+ * @param dimension sets the dimensions of the browser window
+ * @param browser selects the browser to run the tracking session
+ */
+class PageTracker(private val page: URL,
                   private val dimension: Dimension? = null,
-                  private val browser: Browser? = null)
-{
+                  private val browser: Browser? = null) {
     private val data = HashMap<String, String>()
     private val selectors = ArrayList<String>()
 
     /**
-     *
+     * selects some elements to track from the page
+     */
+    fun select(queryString: String) {
+        selectors.add(queryString)
+    }
+
+    /**
+     * Tracks the provided selectors for the provided page
      */
     fun track(): Map<String, String> {
         spawnBrowserSession().use {
-            //val wnd = it.driver.manage().window()
-
-            // Viewport
-            val vpWidth = it.driver.executeScript("return window.innerWidth;")
-            val vpHeight = it.driver.executeScript("return window.innerHeight;")
-            data["vp_width"] = vpWidth.toString()
-            data["vp_height"] = vpHeight.toString()
-            println("Viewport: ${vpWidth}x${vpHeight}")
-
-            // Browser frame
-            val wndWidth = it.driver.executeScript("return screen.availWidth;")
-            val wndHeight = it.driver.executeScript("return screen.availHeight;")
-            data["wnd_width"] = wndWidth.toString()
-            data["wnd_height"] = wndHeight.toString()
-            println("Window: ${wndWidth}x${vpHeight}")
-
+            fetchMetadata(it.driver)
             Thread.sleep(5_000)
 
             for(selector in selectors) {
                 doSelect(selector, it.driver)
             }
-
             //Thread.sleep(5_000)
         }
         return data
     }
 
-    private fun spawnBrowserSession() = SessionBuilder(targetPage,
-                                                       dimension,
-                                                       browser)
+    private fun spawnBrowserSession() = SessionBuilder(page, dimension, browser)
+
+    private fun fetchMetadata(driver: RemoteWebDriver) {
+        //val wnd = it.driver.manage().window()
+        // Viewport
+        val vpWidth = driver.executeScript("return window.innerWidth;")
+        val vpHeight = driver.executeScript("return window.innerHeight;")
+        data["vp_width"] = vpWidth.toString()
+        data["vp_height"] = vpHeight.toString()
+        println("Viewport: ${vpWidth}x${vpHeight}")
+
+        // Browser frame
+        val wndWidth = driver.executeScript("return screen.availWidth;")
+        val wndHeight = driver.executeScript("return screen.availHeight;")
+        data["wnd_width"] = wndWidth.toString()
+        data["wnd_height"] = wndHeight.toString()
+        println("Window: ${wndWidth}x${vpHeight}")
+    }
 
     private fun doSelect(queryString: String, driver: RemoteWebDriver) {
         val elem = driver.findElement(By.cssSelector(queryString))
@@ -59,13 +70,6 @@ class PageTracker(private val targetPage: URL,
 //        val rect = driver.executeScript(
 //            "return document.querySelector('${queryString}').getBoundingClientRect()")
 //        data["${queryString}::rect"] = rect.toString()
-    }
-
-    /**
-     *
-     */
-    fun select(queryString: String) {
-        selectors.add(queryString)
     }
 
 //    fun selectAll(driver: RemoteWebDriver) {
