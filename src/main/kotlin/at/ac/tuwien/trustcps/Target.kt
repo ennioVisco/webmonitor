70dcaf3bce320cdbeb02a94e6d0bd3c294bbe099
Ennio Visconti <ennio.visconti@gmail.com>
2022-04-26 01:02:22 +0200

    fixed mobile browser

--- src/main/kotlin/at/ac/tuwien/trustcps/Target.kt
+++ src/main/kotlin/at/ac/tuwien/trustcps/Target.kt
@@ -5,7 +5,7 @@ object Target {
     private const val HORIZONTAL_BROWSER_FRAME = 0
     private const val VERTICAL_BROWSER_FRAME = 133
 
-    const val screenWidth = 100 + HORIZONTAL_BROWSER_FRAME
+    const val screenWidth = 200 + HORIZONTAL_BROWSER_FRAME
     const val screenHeight = 100 + VERTICAL_BROWSER_FRAME
 
     const val targetUrl = "https://enniovisco.github.io/webmonitor/"
--- src/main/kotlin/at/ac/tuwien/trustcps/tracking/SessionBuilder.kt
+++ src/main/kotlin/at/ac/tuwien/trustcps/tracking/SessionBuilder.kt
@@ -1,31 +1,39 @@
 package at.ac.tuwien.trustcps.tracking
 
+import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.io.Closeable
 import java.net.URL
-import io.github.bonigarcia.wdm.WebDriverManager
 
-class SessionBuilder(url: URL,
-                     dims: Dimension? = null,
-                     engine: Browser? = Browser.CHROME)
-    : Closeable {
+
+class SessionBuilder(
+    url: URL,
+    dims: Dimension? = null,
+    engine: Browser? = Browser.CHROME
+) : Closeable {
     val driver: RemoteWebDriver = when (engine) {
-            Browser.CHROME -> run {
+        Browser.CHROME -> run {
+            if (dims?.width!! < 500 && dims.height < 400) {
+                WebDriverManager.chromedriver().browserInDockerAndroid().setup()
+                ChromeDriver()
+            } else {
                 WebDriverManager.chromedriver().setup()
                 ChromeDriver()
             }
-            Browser.FIREFOX -> run {
-                WebDriverManager.firefoxdriver().setup()
-                FirefoxDriver()
-            }
+        }
+        Browser.FIREFOX -> run {
+            WebDriverManager.firefoxdriver().setup()
+            FirefoxDriver()
+        }
         else -> unsupportedBrowser()
     }
 
     init {
-        driver.manage().window().size = dims?: Dimension(1920, 1080)
+        driver.manage().window().size = dims ?: Dimension(1920, 1080)
         driver.get(url.toString())
     }
 
@@ -36,5 +44,17 @@ class SessionBuilder(url: URL,
 
     private fun unsupportedBrowser(): RemoteWebDriver {
         throw UnsupportedOperationException("The provided browser is not supported")
+    }
+
+    private fun something() {
+        val mobileEmulation = mapOf(
+            "deviceName" to "Pixel 5"
+        )
+
+        val chromeOptions = ChromeOptions()
+
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation)
+
+        val driver = ChromeDriver(chromeOptions)
     }
 }
