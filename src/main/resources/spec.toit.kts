3c0b13ef8e06fe7d7a34a513020f9dc51fb6e5a7
Ennio Visconti <ennio.visconti@gmail.com>
2023-03-28 16:17:14 +0200

    Minor updates

--- src/main/resources/spec.toit.kts
+++ src/main/resources/spec.toit.kts
@@ -5,9 +5,13 @@ Spec.atoms(
     //read "height"
 //    select { ".left-bordered-title" },  // [1]
     // read "height"
-    select { ".colored-block__title.clearfix" }
+    select { ".colored-block__title.clearfix" },
     // .quick-search--input
 //    select { "a[data-target='global-menu']" }
+
+    select { ".item-meta-row p" } read "height" greaterThan 320,
+    select { ".item-meta-row p" } read "width" greaterThan 256,
+    select { ":is(.item-meta-row p):nth-of-type(1)" },
 )
 
 //Spec.record(
@@ -19,6 +23,11 @@ Spec.atoms(
 val screen = Spec.screen
 val feedback = Spec.atoms[0]
 val title = Spec.atoms[1]
+val pHeight = Spec.atoms[2]
+val pWidth = Spec.atoms[3]
+
+val titleAndFeedbackOverlap = not(title and feedback and screen)
+val reflow = not(pHeight and pWidth)
 
 // Final formula
-Spec.formula = title and feedback and screen
+Spec.formula = titleAndFeedbackOverlap
