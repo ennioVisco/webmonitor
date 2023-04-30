461308842a148e3df1393f9a0adb68a65834b38c
Ennio Visconti <ennio.visconti@gmail.com>
2023-04-30 20:05:31 +0200

    updated parameters handling in cli

--- src/main/kotlin/at/ac/tuwien/trustcps/cli/Cli.kt
+++ src/main/kotlin/at/ac/tuwien/trustcps/cli/Cli.kt
@@ -21,15 +21,11 @@ class Cli(
 
     private fun validateArgs(args: Array<String>) {
         try {
-            val (source, spec) = args
-            loadScripts(source, spec)
-        } catch (e: ArrayIndexOutOfBoundsException) {
-            try {
-                val (source) = args
-                loadScripts(source, spec = source)
-            } catch (e: ArrayIndexOutOfBoundsException) {
-                log.info("No arguments provided, using default scripts")
-                loadScripts(source = "sample", spec = "sample")
+            when (args.size) {
+                0 -> defaultScript()
+                1 -> oneArgument(args)
+                2 -> twoArguments(args)
+                else -> log.info("Using custom scripts")
             }
         } catch (e: NullPointerException) {
             if (args.size > 1)
@@ -39,9 +35,32 @@ class Cli(
         }
     }
 
+    private fun defaultScript() {
+        log.info("No arguments provided, using default scripts")
+        loadScripts(source = "sample", spec = "sample")
+    }
+
+    private fun oneArgument(args: Array<String>) {
+        val (source) = args
+        log.info(
+            "Using 'source.$source.kts' as source script and " +
+                    "'spec.$source.kts' as spec script"
+        )
+        loadScripts(source = source, spec = source)
+    }
+
+    private fun twoArguments(args: Array<String>) {
+        val (source, spec) = args
+        log.info(
+            "Using 'source.$source.kts' as source script and " +
+                    "'spec.$spec.kts' as spec script"
+        )
+        loadScripts(source = source, spec = spec)
+    }
+
     private fun errorMsg(source: String, spec: String) =
-        "Cannot find any files named 'source.${source}.kts' and" +
-                " 'spec.${spec}.kts'. Please make sure that the files exist " +
+        "Cannot find any files named 'source.$source.kts' and" +
+                " 'spec.$spec.kts'. Please make sure that the files exist " +
                 "and are in the 'resources' directory."
 
     private fun loadScripts(source: String, spec: String) {
