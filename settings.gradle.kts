8d38ae319b2e8b45f399300b33411a52366ac9e8
Ennio Visconti <ennio.visconti@gmail.com>
2023-05-08 14:27:16 +0200

    build: moved nyx to CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,35 @@
-import com.mooltiverse.oss.nyx.gradle.*
-
 rootProject.name = "webmonitor"
 
-plugins {
-    id("com.mooltiverse.oss.nyx") version "2.0.0"
-}
+//plugins {
+//    id("com.mooltiverse.oss.nyx") version "2.0.0"
+//}
 
-extensions.configure<NyxExtension>("nyx") {
-    preset.set("simple")
-
-    dryRun.set(false)
-//    resume.set(false)
-//    stateFile.set(".nyx-state.yml")
-    verbosity.set("DEBUG")
-
-    changelog {
-        path.set("build/CHANGELOG.md")
-    }
-}
+//extensions.configure<NyxExtension>("nyx") {
+//    preset.set("simple")
+//
+//    dryRun.set(false)
+////    resume.set(false)
+////    stateFile.set(".nyx-state.yml")
+//    verbosity.set("DEBUG")
+//
+//    changelog {
+//        path.set("build/CHANGELOG.md")
+//    }
+//
+//    releaseTypes {
+//        publicationServices.set(listOf("github"))
+//    }
+//
+//    services {
+////        github {
+////            type.set("GITHUB")
+////            options {
+////                // The authentication token is read from the GH_TOKEN environment variable.
+////                AUTHENTICATION_TOKEN =
+////                    "{{#environmentVariable}}GH_TOKEN{{/environmentVariable}}"
+////                REPOSITORY_NAME.set("webmonitor")
+////                REPOSITORY_OWNER.set("ennioVisco")
+////            }
+////        }
+//    }
+//}
