83541eb320a78beb228d0348fcd4e96a3209d66e
Ennio Visconti <ennio.visconti@gmail.com>
2023-05-09 11:39:09 +0200

    chore: minor code cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,35 +1,41 @@
+import com.mooltiverse.oss.nyx.gradle.*
+
 rootProject.name = "webmonitor"
 
-//plugins {
-//    id("com.mooltiverse.oss.nyx") version "2.0.0"
-//}
+plugins {
+    id("com.mooltiverse.oss.nyx") version "2.0.0"
+}
 
-//extensions.configure<NyxExtension>("nyx") {
-//    preset.set("simple")
-//
-//    dryRun.set(false)
-////    resume.set(false)
-////    stateFile.set(".nyx-state.yml")
-//    verbosity.set("DEBUG")
-//
-//    changelog {
-//        path.set("build/CHANGELOG.md")
-//    }
-//
-//    releaseTypes {
-//        publicationServices.set(listOf("github"))
-//    }
-//
-//    services {
-////        github {
-////            type.set("GITHUB")
-////            options {
-////                // The authentication token is read from the GH_TOKEN environment variable.
-////                AUTHENTICATION_TOKEN =
-////                    "{{#environmentVariable}}GH_TOKEN{{/environmentVariable}}"
-////                REPOSITORY_NAME.set("webmonitor")
-////                REPOSITORY_OWNER.set("ennioVisco")
-////            }
-////        }
-//    }
-//}
+extensions.configure<NyxExtension>("nyx") {
+    preset.set("simple")
+
+    dryRun.set(false)
+//    resume.set(false)
+//    stateFile.set(".nyx-state.yml")
+    verbosity.set("DEBUG")
+
+    changelog {
+        path.set("build/CHANGELOG.md")
+    }
+
+    releaseTypes {
+        publicationServices.set(listOf("github"))
+    }
+
+    withGroovyBuilder {
+
+    }
+
+    services {
+        "github" to closureOf<NyxExtension.ServiceConfiguration> {
+            type.set("GITHUB")
+//            options.set(
+//                mapOf(
+//                    "AUTHENTICATION_TOKEN" to "GITHUB_TOKEN",
+//                    "REPOSITORY_NAME" to "myrepo",
+//                    "REPOSITORY_OWNER" to "acme",
+//                )
+//            )
+        }
+    }
+}
